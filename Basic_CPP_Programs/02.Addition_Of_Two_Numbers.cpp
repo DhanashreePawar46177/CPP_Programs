e13498c23d06d8275d60e90aab47b392f932d553
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-24 23:12:28 +0530

    CPP Program

---
+++ Basic_CPP_Programs/02.Addition_Of_Two_Numbers.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int Num1 = 0, Num2 = 0, Sum = 0;
+
+        cout << "\n Enter 1st Number For Addition = " << endl;
+        cin >> Num1;
+
+        cout << "\n Enter 2nd Number For Addition = " << endl;
+        cin >> Num2;
+
+        Sum = Num1 + Num2;
+
+        cout << "\n Addition Of Two Numbers = " << Sum << endl;
+
+        getch();
+        return 0;
+}
+

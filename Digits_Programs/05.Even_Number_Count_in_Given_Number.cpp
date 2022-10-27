ae5970be37c7a0920a4fd570a84661c0a20fd48e
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-27 21:07:20 +0530

    Digit Program (CPP)

---
+++ Digits_Programs/05.Even_Number_Count_in_Given_Number.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int No = 0, Temp = 0, Dig = 0, eCnt = 0;
+
+        cout << "\n Enter a Number = " << endl;
+        cin >> No;
+
+        Temp = No;
+        while(Temp > 0)
+        {
+                Dig = Temp % 10;
+                Temp = Temp / 10;
+                if(Dig % 2 == 0 && Dig != 0)
+                {
+                    eCnt++;
+                }
+        }
+
+        cout << "\n Even Count in " << No << " = " << eCnt << endl;
+        cout <<"\n Thanks !!!\n" << endl;
+
+        getch();
+        return 0;
+}

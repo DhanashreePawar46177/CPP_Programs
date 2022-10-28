52d97d766c91e301f87d93a59f11ae3910e9dc1c
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-28 21:05:00 +0530

    Digit Program (CPP)

---
+++ Digits_Programs/09.Reverse_Of_Given_Number.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int No = 0, Temp = 0, Dig = 0, Rev = 0;
+
+        Up:
+        cout << "\n Enter Positive Integer Number = " << endl;
+        cin >> No;
+
+        Temp = No;
+        if(Temp <= 0)
+        {
+               cout << "\n Invalid Number. \n Please Enter Positive Integer Number.";
+               getch();
+               system("cls");
+               goto Up;
+        }
+
+        while(Temp > 0)
+        {
+                Dig = Temp % 10;
+                Temp = Temp / 10;
+                Rev = (Rev * 10) + Dig;
+        }
+
+        cout << "\n The Reverse Of " << No << " = " << Rev << endl;
+        cout << "\n Thanks !!!\n" << endl;
+
+        getch();
+        return 0;
+}
+

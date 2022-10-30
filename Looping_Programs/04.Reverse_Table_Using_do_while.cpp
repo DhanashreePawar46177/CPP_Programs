08ece366ec968eef44f3dc3cc56cd5606dad8baf
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-30 20:01:16 +0530

    Looping Program (CPP)

---
+++ Looping_Programs/04.Reverse_Table_Using_do_while.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int No = 0, i = 0;
+
+        cout << "\n Enter Positive Integer Number For Printing Reverse Table = " << endl;
+        cin >> No;
+        cout << "\n Reverse Table Of " << No << " => \n" << endl;
+
+        i = 10;
+        do
+        {
+            cout << " " << No << " * " << i << " = " << No*i << endl;
+            i--;
+        }while(i >= 1);
+
+        cout << "\n Thanks !!!\n" << endl;
+
+        getch();
+        return 0;
+}

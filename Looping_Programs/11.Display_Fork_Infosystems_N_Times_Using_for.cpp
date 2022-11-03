301eb2685e5cea27e63c25a184da9522f0cb31bc
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-11-03 22:01:04 +0530

    Looping Program (CPP)

---
+++ Looping_Programs/11.Display_Fork_Infosystems_N_Times_Using_for.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int No = 0, Cnt = 0;
+
+        cout << "\n\n Enter Positive Integer Number For Printing Class Name = ";
+        cin >> No;
+
+        cout << "\n===================================================================" << endl;
+
+        for(Cnt = 1; Cnt <= No; Cnt++)
+        {
+                cout << "\n\t\t\t FORK INFOSYSTEMS " << endl;
+        }
+
+        cout << "\n===================================================================" << endl;
+
+        cout << "\n Thanks !!!\n" << endl;
+
+        getch();
+        return 0;
+}

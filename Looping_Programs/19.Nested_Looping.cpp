3d326e61e7d8238acca213a361c49d2ffa73693d
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-11-23 21:47:21 +0530

    Looping  Program(CPP)

---
+++ Looping_Programs/19.Nested_Looping.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int i = 0, j = 0, k = 0;
+
+        cout << "\n\t Welcome To Class" << endl;
+
+        for(i = 1 ; i <= 4 ; i++)
+        {
+              k = 0;
+              for(j = 1 ; j <= 5 ; j++)
+              {
+                    cout << "\n\t FORK INFSYSTEMS " << endl;
+
+              }
+              k = k+4;
+              cout << "\n\t  Karad" << endl;
+        }
+        cout << "\n\n\t Thanks !!!" << endl;
+
+        getch();
+        return 0;
+}

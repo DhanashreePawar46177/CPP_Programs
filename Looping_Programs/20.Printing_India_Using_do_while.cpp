98cf3842783c88b5fc150290d912590a4fcd7302
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-12-13 14:37:51 +0530

    Lopping Program(CPP)

---
+++ Looping_Programs/20.Printing_India_Using_do_while.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+         int i = 1;
+
+         cout << "\n\n\t=========********=========\n" << endl;
+         do
+         {
+              cout << "\n\t\t India" << endl;
+              i++;
+         }while(i<=5);
+
+         cout << "\n\n\t=========********=========\n" << endl;
+
+         cout << "\n\t Thanks !!!\n" << endl;
+
+         getch();
+         return 0;
+}

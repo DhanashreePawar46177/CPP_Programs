eb587c5e74ece9431856f6387c79b98d6f773ce8
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2023-12-27 10:40:31 +0530

    CPP Program

---
+++ Basic_CPP_Programs/03.Subtraction_Of_Two_Numbers.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int Num1 = 0, Num2 = 0, Sub = 0;
+
+        cout << "\n Enter 1st Number For Subtraction = " << endl;
+        cin >> Num1;
+
+        cout << "\n Enter 2nd Number For Subtraction = " << endl;
+        cin >> Num2;
+
+        Sub = Num1 - Num2;
+
+        cout << "\n Subtraction " << Num1 << " and " << Num2 << " = " << Sub << endl;
+
+        cout << "\n Thanks !!!\n" << endl;
+
+        getch();
+        return 0;
+}
